68f17f1d34faa0faf386371070f15850ef5b5593
AmeliaVioletaH <2513422017@mail.utec.edu.sv>
2022-04-29 14:20:25 -0600

    reconocimiento de las categorias

--- SAIModelo/Conexion.cs
+++ SAIModelo/Conexion.cs
@@ -18,11 +18,13 @@ namespace SAIModelo
         //private const string servidor = "DESKTOP-LVOSUC0";
 
         //Coexion Angel
-        private const string servidor = "DESKTOP-37CLJ35";
+        //private const string servidor = "DESKTOP-37CLJ35";
 
         //Coexion Moris
-       // private const string servidor = "DESKTOP-D8EPUG8";
-        
+        // private const string servidor = "DESKTOP-D8EPUG8";
+
+        //Conexion Violeta
+        private const string servidor = "DESKTOP-8SBOFT9";
 
         private const string DBName = "SAIDB";
 
